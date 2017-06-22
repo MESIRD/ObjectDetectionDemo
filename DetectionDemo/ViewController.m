2910d5a305bfe0ffd508d056f8745e58bd47b89d
ajie <ajie@maihaoche.com>
2017-06-22 14:53:23 +0800

    first commit

--- DetectionDemo/ViewController.m
+++ DetectionDemo/ViewController.m
@@ -8,8 +8,22 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import <AVFoundation/AVFoundation.h>
+#import "Resnet50.h"
 
+static inline double radians (double degrees) { return degrees * M_PI/180; }
+
+@interface ViewController () <AVCaptureVideoDataOutputSampleBufferDelegate>
+
+@property (nonatomic, strong) AVCaptureSession *captureSession;
+@property (nonatomic, strong) AVCaptureDeviceInput *captureDeviceInput;
+@property (nonatomic, strong) AVCaptureVideoDataOutput *captureVideoOutput;
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *captureVideoPreviewLayer;
+
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (weak, nonatomic) IBOutlet UILabel *infoLabel;
+
+@property (nonatomic, strong) Resnet50 *resnet50Model;
 @end
 
 @implementation ViewController
@@ -17,8 +31,153 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self _setUpCamera];
+    _resnet50Model = [[Resnet50 alloc] init];
 }
 
+- (void)_setUpCamera {
+    
+    _captureSession = [[AVCaptureSession alloc] init];
+    if ([_captureSession canSetSessionPreset:AVCaptureSessionPreset1920x1080]) {
+        [_captureSession canSetSessionPreset:AVCaptureSessionPreset1920x1080];
+    }
+    
+    AVCaptureDeviceDiscoverySession *deviceDiscoverySession = [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes:@[AVCaptureDeviceTypeBuiltInWideAngleCamera, AVCaptureDeviceTypeBuiltInTelephotoCamera, AVCaptureDeviceTypeBuiltInDualCamera] mediaType:AVMediaTypeVideo position:AVCaptureDevicePositionBack];
+    if (deviceDiscoverySession.devices.count == 0) {
+        return;
+    }
+    
+    AVCaptureDevice *captureDevice = deviceDiscoverySession.devices.firstObject;
+    
+    NSError *inputError = nil;
+    _captureDeviceInput = [[AVCaptureDeviceInput alloc] initWithDevice:captureDevice error:&inputError];
+    if (inputError) {
+        NSLog(@"error occured while set device input...");
+        return;
+    }
+    
+    _captureVideoOutput = [[AVCaptureVideoDataOutput alloc] init];
+    [_captureVideoOutput setAlwaysDiscardsLateVideoFrames:YES];
+    _captureVideoOutput.videoSettings = [NSDictionary dictionaryWithObject: [NSNumber numberWithInt:kCVPixelFormatType_32BGRA] forKey: (id)kCVPixelBufferPixelFormatTypeKey];
+    dispatch_queue_t queue = dispatch_queue_create("VideoQueue", DISPATCH_QUEUE_SERIAL);
+    [_captureVideoOutput setSampleBufferDelegate:self queue:queue];
+    
+    //将设备输入添加到会话中
+    if ([_captureSession canAddInput:_captureDeviceInput]) {
+        [_captureSession addInput:_captureDeviceInput];
+    }
+    
+    //将设备输出添加到会话中
+    if ([_captureSession canAddOutput:_captureVideoOutput]) {
+        [_captureSession addOutput:_captureVideoOutput];
+    }
+    
+    _containerView.layer.masksToBounds = YES;
+    
+    _captureVideoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:_captureSession];
+    _captureVideoPreviewLayer.frame = _containerView.layer.bounds;
+    _captureVideoPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    
+    [_containerView.layer insertSublayer:_captureVideoPreviewLayer atIndex:0];
+    
+    [_captureSession startRunning];
+}
+
+- (void)captureOutput:(AVCaptureOutput *)output didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
+    UIImage *image = [self imageFromSampleBuffer:sampleBuffer];
+    CVPixelBufferRef pixelBuffer = [self pixelBufferFromCGImage:image.CGImage];
+    NSError *predictionError = nil;
+    Resnet50Output *resnet50Output = [_resnet50Model predictionFromImage:pixelBuffer error:&predictionError];
+    
+    __weak __typeof(self) weakSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSString *text = [NSString stringWithFormat:@"%@(%@)", resnet50Output.classLabel, resnet50Output.classLabelProbs[resnet50Output.classLabel]];
+        weakSelf.infoLabel.text = text;
+    });
+    sleep(1);
+}
+
+- (UIImage *)imageFromSampleBuffer:(CMSampleBufferRef)sampleBuffer {
+    // Get a CMSampleBuffer's Core Video image buffer for the media data
+    CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    // Lock the base address of the pixel buffer
+    CVPixelBufferLockBaseAddress(imageBuffer, 0);
+    
+    // Get the number of bytes per row for the pixel buffer
+    void *baseAddress = CVPixelBufferGetBaseAddress(imageBuffer);
+    
+    // Get the number of bytes per row for the pixel buffer
+    size_t bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer);
+    // Get the pixel buffer width and height
+    size_t width = CVPixelBufferGetWidth(imageBuffer);
+    size_t height = CVPixelBufferGetHeight(imageBuffer);
+    
+    // Create a device-dependent RGB color space
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    
+    // Create a bitmap graphics context with the sample buffer data
+    CGContextRef context = CGBitmapContextCreate(baseAddress, width, height, 8,
+                                                 bytesPerRow, colorSpace, kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
+    // Create a Quartz image from the pixel data in the bitmap graphics context
+    CGImageRef quartzImage = CGBitmapContextCreateImage(context);
+    
+    CGRect cropFrame = CGRectMake((width - 1080)/2, (height - 1080)/2, 1080, 1080);
+    CGImageRef imageRef = CGImageCreateWithImageInRect(quartzImage, cropFrame);
+    UIGraphicsBeginImageContext(CGSizeMake(224, 224));
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextTranslateCTM(ctx, 112, 112);
+    CGContextScaleCTM(ctx, -1, 1);
+    CGContextRotateCTM(ctx, radians(90));
+    CGContextTranslateCTM(ctx, -112, -112);
+    CGContextDrawImage(ctx, CGRectMake(0, 0, 224, 224), imageRef);
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    CGImageRelease(imageRef);
+    UIGraphicsEndImageContext();
+ 
+    // Unlock the pixel buffer
+    CVPixelBufferUnlockBaseAddress(imageBuffer,0);
+    
+    // Free up the context and color space
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+    
+    // Release the Quartz image
+    CGImageRelease(quartzImage);
+    return (newImage);
+}
+
+- (CVPixelBufferRef)pixelBufferFromCGImage:(CGImageRef)image {
+    
+    CGSize frameSize = CGSizeMake(CGImageGetWidth(image), CGImageGetHeight(image));
+    NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
+                             [NSNumber numberWithBool:NO], kCVPixelBufferCGImageCompatibilityKey,
+                             [NSNumber numberWithBool:NO], kCVPixelBufferCGBitmapContextCompatibilityKey,
+                             nil];
+    CVPixelBufferRef pxbuffer = NULL;
+    CVReturn status = CVPixelBufferCreate(kCFAllocatorDefault, frameSize.width,
+                                          frameSize.height,  kCVPixelFormatType_32ARGB, (__bridge CFDictionaryRef) options,
+                                          &pxbuffer);
+    NSParameterAssert(status == kCVReturnSuccess && pxbuffer != NULL);
+    
+    CVPixelBufferLockBaseAddress(pxbuffer, 0);
+    void *pxdata = CVPixelBufferGetBaseAddress(pxbuffer);
+    
+    
+    CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(pxdata, frameSize.width,
+                                                 frameSize.height, 8, 4*frameSize.width, rgbColorSpace,
+                                                 kCGImageAlphaNoneSkipLast);
+    
+    CGContextDrawImage(context, CGRectMake(0, 0, CGImageGetWidth(image),
+                                           CGImageGetHeight(image)), image);
+    CGColorSpaceRelease(rgbColorSpace);
+    CGContextRelease(context);
+    
+    CVPixelBufferUnlockBaseAddress(pxbuffer, 0);
+    
+    return pxbuffer;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
