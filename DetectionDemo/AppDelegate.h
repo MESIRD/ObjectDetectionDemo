75ee1ffb562d563768911960022b6d39495f34c7
ajie <ajie@maihaoche.com>
2017-06-22 09:34:31 +0800

    Initial Commit

---
+++ DetectionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DetectionDemo
+//
+//  Created by xujie on 6/22/17.
+//  Copyright © 2017 xujie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DetectionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DetectionDemo
+//
+//  Created by xujie on 6/22/17.
+//  Copyright © 2017 xujie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
